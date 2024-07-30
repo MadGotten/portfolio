ff23f16f5806d5e034083aee081fd730b73e6764
Marek Stefaniak <madgotten@wp.pl>
2024-07-30 17:15:31 +0200

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Marek Stefaniak",
+  description: "Portfolio of Marek Stefaniak",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
