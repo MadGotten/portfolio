b985ba86e646eab90e335cf7f4a6320fc82d01c6
Marek Stefaniak <madgotten@wp.pl>
2025-06-17 21:27:29 +0200

    Fix cursor changing when hovering over text

--- components/tech-stack.tsx
+++ components/tech-stack.tsx
@@ -26,7 +26,7 @@ const TechItem = ({ Icon, name }: TechItemProps) => {
     <div
       className="px-4 sm:px-5 py-3 sm:py-4 border border-yellow border-opacity-50 bg-yellow bg-opacity-20 hover:bg-opacity-40 hover:border-opacity-70 
       dark:border-opacity-30 dark:bg-opacity-5 dark:hover:bg-opacity-10 dark:hover:border-opacity-35 
-      rounded text-center flex items-center justify-center sm:justify-start gap-4"
+      rounded text-center flex items-center justify-center sm:justify-start gap-4 cursor-default"
     >
       <Icon />
       {name}
